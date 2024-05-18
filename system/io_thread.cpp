236837eef4af27f11ae01023fe94b9c8fc198180
Shengqi <shengqi647@gmail.com>
2024-05-19 05:40:33 +0800

    pt

--- system/io_thread.cpp
+++ system/io_thread.cpp
@@ -14,6 +14,8 @@
 #include "timer.h"
 //#include "crypto.h"
 
+bool initialized = false;
+
 void InputThread::managekey(KeyExchange *keyex)
 {
     string algorithm = keyex->pkey.substr(0, 4);
@@ -107,7 +109,7 @@ void InputThread::setup()
                     if (msg->rtype == CL_BATCH)
                     {
 #if PERSISTENT_COUNTER
-                while (!get_and_inc_next_idx((ClientQueryBatch *)msg, msg->txn_id));
+                // while (!get_and_inc_next_idx((ClientQueryBatch *)msg, msg->txn_id));
 #elif SGX
                         msg->txn_id = get_and_inc_next_idx((ClientQueryBatch *)msg);
 #else
@@ -362,7 +364,14 @@ RC InputThread::server_recv_loop()
             if (msg->rtype == CL_BATCH)
             {
 #if PERSISTENT_COUNTER
-                while (!get_and_inc_next_idx((ClientQueryBatch *)msg, msg->txn_id));
+                // while (!get_and_inc_next_idx((ClientQueryBatch *)msg, msg->txn_id));
+                if(!initialized) {
+                    system("tpm2_nvundefine 0x1500016 > /dev/null 2>&1");
+                    system("tpm2_nvdefine -C o -s 8 -a \"ownerread|authread|authwrite|nt=1\" 0x1500016 -p index");
+                    initialized = true;
+                }
+                system("tpm2_nvincrement -C 0x1500016 0x1500016 -P \"index\" > /dev/null 2>&1");
+
 #elif SGX
                 msg->txn_id = get_and_inc_next_idx((ClientQueryBatch *)msg);
 #else
